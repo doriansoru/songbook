67f6b1d152472ad2b5fbbe4b9e3cb7bc7b08ac95
doriansoru <doriansoru@gmail.com>
2021-12-29 08:31:35 +0100

    Updated the example

--- create_songbook.py
+++ create_songbook.py
@@ -7,15 +7,17 @@ example_folder = 'example'
 my_book_name = 'MesseBreve'
 
 # Order by number. To order by title use OrderType.TITLE
-book = Songbook(book_name=my_book_name, number_of_song_digits=3, scores_subdir='scores', order=OrderType.NUMBER)
+book = Songbook(book_name=my_book_name, number_of_song_digits=3, scores_subdir='example/scores', order=OrderType.NUMBER)
 book.build()
 print('Ok, build completed.')
 compile=input('Do I compile the book with pdflatex? [Y]/n ').lower()
 # Compile twice to build the Table of Contents
 if compile == '' or  compile == 'y':
-	# Move the file into the example folder
-	os.rename('.'.join([my_book_name, 'tex']), os.sep.join([example_folder, '.'.join([my_book_name, 'tex'])]))
-	os.chdir(example_folder)
 	for i in range(2):
 		os.system(' '.join(['pdflatex', my_book_name]))
+	# Move the file into the example folder
+	files = os.listdir('.')
+	for f in files:
+		if f.startswith(my_book_name):
+			os.rename(f, os.sep.join([example_folder, f]))
 	print(' '.join([os.sep.join([example_folder, '.'.join([my_book_name, 'pdf'])]), 'compiled correctly']))
